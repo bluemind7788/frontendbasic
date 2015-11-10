179cdae28f5cf2d73f5d4459f4edaceaa8ac81cf
tianxq1 <tianxq1@yonyou.com>
2015-11-10 11:12:57 +0800

    1

--- underscore_t.js
+++ underscore_t.js
@@ -15,16 +15,29 @@ _.clone = function(obj) {
 	
 	if(typeof obj !== 'object') {
 		return obj;
-	} else {
-		if(obj == null) {
-			return null
+	} else if(obj == null) {
+		return null;
+	} else  {
+		var cloneobj;
+		if(Object.prototype.toString.call(obj) == '[object Array]') {
+			cloneobj = []
 		} else {
-			var cloneobj = {}
-			for(var k in obj) {
-				cloneobj[k] = _.clone(obj[k])
-			}
-			return cloneobj;
+			cloneobj = {}
 		}
+		
+		for(var k in obj) {
+			cloneobj[k] = _.clone(obj[k])
+		}
+		return cloneobj;
 	}
-	
 }
+
+_.humpStr = function(str) {
+	var reg = /-(\w)/g
+	str = str.replace(reg, function($0, $1) {
+		return $1.toUpperCase()
+	})
+	return str;
+}
+
+
